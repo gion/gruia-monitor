8bb6e5561111a7fc51499072d4bb76e0d0a07657
gion <bogdan.gradinariu@thinslices.com>
2017-05-26 10:07:19 +0300

    Add the deploy method

--- helpers.sh
+++ helpers.sh
@@ -1,11 +1,31 @@
  #!/bin/bash
 
 function deploy() {
+
+  # get the current branch name
+  branch_name=$(git symbolic-ref -q HEAD)
+  branch_name=${branch_name##refs/heads/}
+  branch_name=${branch_name:-HEAD}
+
   echo "starting to deploy"
+
+  # checkout to a new feature branch
   git checkout -b feature/deploy-$RANDOM
+
+  # add the changes in the public area
   git add public
+
+  # commit the changes
   git commit -m "auto deploy to github pages"
+
+  # push the branch upstream
   git push -u
-  hub pull-request -b github-pages
+
+  # make and open the pull request
+  hub pull-request -o -b gh-pages
+
+  # checkout to the previous branch
+  git checkout $branch_name
+
   echo "merge the pull request in order to have this build deployed on github pages!"
 }
