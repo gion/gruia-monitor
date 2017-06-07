40f7981b3ca8c57d570520686cf69cd8120541fc
gion <bogdan.gradinariu@thinslices.com>
2017-06-07 14:10:03 +0300

    Look at the `<body>` element at it's attributes when triggering motion/noise  - try to integrate google charts  - add sound detection  - add motion detection  - fix readme typo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import app from './app'
 import * as style from './style.scss'
 
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {packages: ['corechart']})
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(app.initChart)
+
 window.app = app
